b0742cd96f83aca3aa99cc621c30e6599abd4c9b
= <biphox@gmail.com>
2019-01-15 11:18:10 +0300

    OAuth2 Support: Authentication Server * draft

--- src/main/java/net/data/repository/MonsterRepository.java
+++ src/main/java/net/data/repository/MonsterRepository.java
@@ -1,7 +1,10 @@
 package net.data.repository;
 
 import net.data.model.entity.Monster;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
@@ -12,6 +15,9 @@ public class MonsterRepository {
     @PersistenceContext
     private EntityManager em;
 
+
+    @Transactional(readOnly = true)
+    @PreAuthorize("hasAuthority('MONSTER_READ')")
     public List<Monster> list(){
         return em.createQuery("select m from Monster m", Monster.class).getResultList();
     }
--- src/main/java/net/data/security/server/ResourceServerConfig.java
+++ src/main/java/net/data/security/server/ResourceServerConfig.java
@@ -1,6 +1,7 @@
 package net.data.security.server;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
@@ -9,14 +10,20 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-    private static final String RESOURCE_ID = "data-rest-ui";
-    private static final String SECURED_PATTERN = "/api/**";
-
+    private static final String RESOURCE_ID = "data-rest-api";
+    private static final String RESOURCE_READ_SCOPE = "#oauth2.hasScope('read')";
+    private static final String RESOURCE_PATTERN = "/api/**";
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
         resources.resourceId(RESOURCE_ID);
     }
 
-
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.requestMatchers()
+            .antMatchers(RESOURCE_PATTERN)
+            .and()
+            .authorizeRequests().anyRequest().access(RESOURCE_READ_SCOPE);
+    }
 }
